f062086d0d2d08292befeb8a4864bbb154a81f5e
Alex Santos <alexdsantosv@gmail.com>
2023-02-07 19:26:40 -0600

    [ Fix ] loader

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -3,11 +3,14 @@ import React, { Suspense } from "react";
 // 3JS
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
+import { Html, useProgress } from "@react-three/drei";
 
 // COMPONENTS
 import Character from "components/models/character";
 
 const App: React.FC = () => {
+  const { progress } = useProgress();
+
   return (
     <main>
       <Canvas
@@ -15,17 +18,17 @@ const App: React.FC = () => {
           fov: 18,
         }}
         style={{
-          background: "#000",
+          background: "#fff",
           height: "100vh",
           width: "100vw",
         }}
       >
         <ambientLight intensity={0.8} />
-        <Suspense fallback={null}>
+        <Suspense fallback={<Html center>{progress} % loaded</Html>}>
           <Character />
+          <Environment files="/models/skybox/bosque.hdr" background />
         </Suspense>
         <OrbitControls autoRotate />
-        <Environment files="/models/skybox/bosque.hdr" background />
       </Canvas>
     </main>
   );
